e0792ac6abc8c4a6198a3004809848e2bbf17cb1
Elithon Silva <elithonsilva@gmail.com>
2022-07-15 10:29:47 -0300

    o método txt_importer retorna uma lista contendo as linhas do arquivo

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,5 +1,16 @@
+import readline
 import sys
 
 def txt_importer(path_file):
+    file = list()
+
     if ".txt" not in path_file:
         return print("Formato inválido", file=sys.stderr)
+
+    lines = open(path_file).readlines()
+
+    for line in lines:
+        line_break = line.split('\n')
+        file.append(line_break[0])
+
+    return file
