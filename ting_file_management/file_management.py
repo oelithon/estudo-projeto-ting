fa29e4ae23944521a62278488ea2d8735cb102be
Elithon Silva <elithonsilva@gmail.com>
2022-07-15 10:44:47 -0300

    corrige erros lint

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,18 +1,16 @@
 import sys
 
+
 def txt_importer(path_file):
     file = list()
 
     if ".txt" not in path_file:
         return print("Formato inválido", file=sys.stderr)
-
     try:
         lines = open(path_file).readlines() #https://www.w3schools.com/python/ref_file_readlines.asp
-
         for line in lines:
             line_break = line.split('\n')
             file.append(line_break[0])
-
     except FileNotFoundError:
         return print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
 
