26977be21ec7a9beb9df16412be9b3e15f99b605
Elithon Silva <elithonsilva@gmail.com>
2022-07-17 15:03:54 -0300

    Implemente uma função process, capaz de ler o arquivo carregado na função anterior e efetuar o pré-processamento do conteúdo

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,24 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    path_list = list()
+    len_instance = len(instance)
+
+    for item in range(0, len_instance):
+        path_list.append(instance.search(item))
+
+    if path_file not in path_list:
+        file_content = txt_importer(path_file)
+        result = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(file_content),
+            "linhas_do_arquivo": file_content
+        }
+        instance.enqueue(path_file)
+
+        return print(str(result), file=sys.stdout)
 
 
 def remove(instance):
